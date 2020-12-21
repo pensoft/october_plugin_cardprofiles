d4e69693314cdc6f803259ddae6e2d39519d0944
root <root@project-dev.pensoft.local>
2020-12-21 12:57:48 +0200

    update migrations

--- updates/builder_table_create_pensoft_cardprofiles_category.php
+++ updates/builder_table_create_pensoft_cardprofiles_category.php
@@ -7,20 +7,24 @@ class BuilderTableCreatePensoftCardprofilesCategory extends Migration
 {
     public function up()
     {
-        Schema::create('pensoft_cardprofiles_category', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id')->unsigned();
-            $table->string('name', 255);
-            $table->string('slug', 255);
-            $table->text('body')->nullable();
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-        });
+        if (!Schema::hasTable('pensoft_cardprofiles_category')) {
+            Schema::create('pensoft_cardprofiles_category', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('name', 255);
+                $table->string('slug', 255);
+                $table->text('body')->nullable();
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+            });
+        }
     }
     
     public function down()
     {
-        Schema::dropIfExists('pensoft_cardprofiles_category');
+        if (Schema::hasTable('pensoft_cardprofiles_category')) {
+            Schema::dropIfExists('pensoft_cardprofiles_category');
+        }
     }
 }
--- updates/builder_table_create_pensoft_cardprofiles_items.php
+++ updates/builder_table_create_pensoft_cardprofiles_items.php
@@ -7,26 +7,30 @@ class BuilderTableCreatePensoftCardprofilesItems extends Migration
 {
     public function up()
     {
-        Schema::create('pensoft_cardprofiles_items', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id')->unsigned();
-            $table->string('names', 255);
-            $table->text('content')->nullable();
-            $table->integer('category_id');
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-            $table->integer('country_id')->nullable();
-            $table->string('position', 255)->nullable();
-            $table->string('slug');
-            $table->string('department')->nullable();
-            $table->string('email')->nullable();
-
-        });
+        if (!Schema::hasTable('pensoft_cardprofiles_items')) {
+            Schema::create('pensoft_cardprofiles_items', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('names', 255);
+                $table->text('content')->nullable();
+                $table->integer('category_id');
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+                $table->integer('country_id')->nullable();
+                $table->string('position', 255)->nullable();
+                $table->string('slug');
+                $table->string('department')->nullable();
+                $table->string('email')->nullable();
+    
+            });
+        }
     }
     
     public function down()
     {
-        Schema::dropIfExists('pensoft_cardprofiles_items');
+        if (Schema::hasTable('pensoft_cardprofiles_items')) {
+            Schema::dropIfExists('pensoft_cardprofiles_items');
+        }
     }
 }
