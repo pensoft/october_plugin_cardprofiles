b3a9c1f1c4ee4d100f7296d3026b42e8137cefca
root <root@project-dev.pensoft.local>
2021-08-17 09:21:25 +0300

    str replace email

---
+++ updates/builder_table_update_pensoft_cardprofiles_items.php
@@ -0,0 +1,25 @@
+<?php namespace Pensoft\Cardprofiles\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftCardprofilesItems extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_cardprofiles_items', function($table)
+        {
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_cardprofiles_items', function($table)
+        {
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+        });
+    }
+}
